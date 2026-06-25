86ee3e4e39c5a398fda6549fe203046420d493af
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2026-06-25 09:49:33 +0300

    Updated marks

--- 13_student_mark_analyzer/main.py
+++ 13_student_mark_analyzer/main.py
@@ -1,59 +1,49 @@
 
 
-def average_mark(students_list):
-    total_mark = 0
-    highest_mark= None
-    lowest_mark = None
+def student_report(students):
+    marks = list(students.values())
+    total_mark = sum(marks)
+    average_mark = total_mark / len(marks)
+    highest_mark = max(marks)
+    lowest_mark = min(marks)
+    return average_mark, highest_mark, lowest_mark
 
-    for students in students_list:
-        student_mark = int(students["mark"])
-        if lowest_mark == None or student_mark < lowest_mark:
-            lowest_mark = student_mark
-        if highest_mark == None or student_mark > highest_mark:
-            highest_mark = student_mark
-        total_mark += student_mark
-
-    print(f"""
-    Average Marks : {total_mark / len(students_list):.2f}
-    Highest Marks : {highest_mark}
-    Lowest Marks : {lowest_mark}
-    """)
-
-    return total_mark / len(students_list) ,highest_mark,lowest_mark
-
-def view_students(students_list):
+def view_students(students):
+    students_list = list(students.items())
     if len(students_list) == 0:
         print("No students found.")
         return
 
     for student in students_list:
-        print(f"Name: {student['name']}, Mark: {student['mark']}")
+        print(f"Name: {student[0]}, Mark: {student[1]}")
 
-def add_students():
-    students_list = []
+def collect_students_details():
+    students = {}
     while True:
         name = input("Enter the name of the student: ")
-        mark = input("Enter the mark of the student: ")
-
-        isStudentExists = False
-        for student in students_list:
-            if student["name"] == name:
-                print("Student already exists.")
-                isStudentExists = True
-                break
-
-        if not isStudentExists:
-            students_list.append({"name": name, "mark": mark})
+        already_exist_student = [name for student in students.keys() if student == name]
+        
+        if already_exist_student:
+            print("Student already exist")
+            continue
+        
+        mark = int(input("Enter the mark of the student: "))
+        students[name] = mark
         
         isExit = input("Do you want to exit? (y/n): ") == "y"
         if isExit:
             break
 
-    return students_list    
+    return students   
 
 def main():
-    students_list = add_students()
+    students_list = collect_students_details()
     view_students(students_list)
-    average_mark(students_list)
+    average_mark, highest_mark, lowest_mark = student_report(students_list)
+    print(f"Average Marks : {average_mark:.2f}")
+    print(f"Highest Marks : {highest_mark}")
+    print(f"Lowest Marks : {lowest_mark}")
+    print("Thank you for using the student mark analyzer.")
 
-main()
+if __name__ == "__main__":
+    main()
